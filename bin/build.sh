354b07ca900a47fb597b6815b9b1fa067136f4e1
timothyhay <jamesgene@icloud.com>
2025-04-14 23:35:23 -0600

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh thinkpad.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh thinkpad_updGOP.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
